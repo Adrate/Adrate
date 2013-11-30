a803f81ee023c1340a9cf23bc9efc86b6df1d91a
Andrew Childs <dazaris@gmail.com>
2013-11-30 18:16:57 +1030

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adrate::Application.config.secret_key_base = '816d63472ae284a1052feb5a946ca708fdc421d58c1cccb5cb3fa25a7a3be16b859b4410606b051d534b397b5d3f5e83bd37b1db6f0dc07b2b937978e54fa86f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adrate::Application.config.session_store :cookie_store, key: '_Adrate_session'
